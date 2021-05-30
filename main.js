336847e1f4e3e56c1e4c580f982e3239167c6bd7
s-infinity21 <83367007+s-infinity21@users.noreply.github.com>
2021-05-30 18:21:23 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 function myFunction() {
 	var nav = document.getElementById('navbar');
-	var menu = document.getElementById('menu');
-
 	window.addEventListener('scroll', function () {
 		if (this.scrollY > 20) {
 			nav.classList.add('sticky');
@@ -9,8 +7,30 @@ function myFunction() {
 			nav.classList.remove('sticky');
 		}
 	});
-
-	window.addEventListener('click', function () {
-		menu.classList.toggle('active');
-	});
 }
+
+$('.menu-btn').click(function () {
+	$('.navbar .menu').toggleClass('active');
+	$('.menu-btn i').toggleClass('active');
+});
+
+$('.carousel').owlCarousel({
+	margin: 20,
+	loop: true,
+	autoplayTimeOut: 2000,
+	autoplayHoverPause: true,
+	responsive: {
+		0: {
+			items: 1,
+			nav: false
+		},
+		600: {
+			items: 2,
+			nav: false
+		},
+		1000: {
+			items: 3,
+			nav: false
+		}
+	}
+});
